b255003ffc428b2437e00d579437ff1a20a8153a
AnshikaR27 <94916495+AnshikaR27@users.noreply.github.com>
2025-02-18 22:08:03 +0530

    Added global variable to each of the functions

--- bank.py
+++ bank.py
@@ -1,15 +1,17 @@
+balance = 0
 
 def main():
-    balance = 0 #this approach would not work because yeh variable phir wapas is only accessible in the main function; will not be accessible to the deposit and the withdraw function
     print("Balance:", balance)
     deposit(100)
     withdraw(50)
     print("Balance:", balance)
 
 def deposit(n):
+    global balance
     balance += n
 
 def withdraw(n):
+    global balance
     balance -= n
 
 if __name__ == "__main__":
