b23493c9dd3b4eaa7b88c62315f9e64af0701580
AnshikaR27 <94916495+AnshikaR27@users.noreply.github.com>
2025-02-19 02:28:07 +0530

    Learnt about sys.argv for CLI arguments

--- meow.py
+++ meow.py
@@ -1,16 +1,11 @@
-def meow(n:int) -> str:
-    # documenting a function
-    '''
-    Meow n times.
+import sys
 
-    :param n: Number of times to meow
-    :type n: int
-    :raise TypeError: If n is not an int
-    :return: A string of n meows, one per line
-    :rtype: str
-    '''
-    return "meow\n" * n
+if len(sys.argv) == 1:
+    print("meow")
+elif len(sys.argv) == 3 and sys.argv[1] == "-n":
+    n = int(sys.argv[2])
+    for _ in range(n):
+        print("meow")
 
-number: int = int(input("Number: "))
-meows: str = meow(number)
-print(meows, end = "")
+else:
+    print("usage: meows.py")
