ea3ea41baba3e1fd3c62e1dadc6ff278c4634c4c
AnshikaR27 <94916495+AnshikaR27@users.noreply.github.com>
2025-02-18 20:34:32 +0530

    Tried another approach of declaring the variable inside the main func but this did not seem to work as well.

--- bank.py
+++ bank.py
@@ -1,6 +1,6 @@
-balance = 0
 
 def main():
+    balance = 0 #this approach would not work because yeh variable phir wapas is only accessible in the main function; will not be accessible to the deposit and the withdraw function
     print("Balance:", balance)
     deposit(100)
     withdraw(50)
