ccdc036dc6e46be693bf7b098b1810be38e69fa2
AnshikaR27 <94916495+AnshikaR27@users.noreply.github.com>
2025-02-18 22:30:39 +0530

    Added a meow.py function to learn more about constants

---
+++ meow.py
@@ -0,0 +1,8 @@
+MEOWS = 3
+
+MEOWS = 4
+
+for _ in range(MEOWS):
+    print("meow")
+
+# Python me aisa kuch hai nahi as such; ki constant wagere. matlb aisa convention hai ki constants ko tum jisme store karahe ho usse caps me likhte ho.
