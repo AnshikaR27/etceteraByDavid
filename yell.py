f63f138ef1f5192f6266cc1841b45810ce4724bf
AnshikaR27 <94916495+AnshikaR27@users.noreply.github.com>
2025-02-19 12:26:19 +0530

    Making yell function work like the print function by pass input not as a list but multiple args; and this was done by *args

--- yell.py
+++ yell.py
@@ -1,7 +1,7 @@
 def main():
-    yell(["This", "is", "CS50"])
+    yell("This", "is", "CS50")
 
-def yell(words):
+def yell(*words):
     uppercased = []
     for word in words:
         uppercased.append(word.upper())
