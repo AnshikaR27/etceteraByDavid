a47ccbdeb04eb342810d3d31bbbef7aefa165c3b
AnshikaR27 <94916495+AnshikaR27@users.noreply.github.com>
2025-02-19 14:56:36 +0530

    Using the map function to uppercase every string that I pass to the yell func

--- yell.py
+++ yell.py
@@ -2,9 +2,8 @@ def main():
     yell("This", "is", "CS50")
 
 def yell(*words):
-    uppercased = []
-    for word in words:
-        uppercased.append(word.upper())
+    # just passing the upper func; not calling it but just passing it phir map func khud call karega
+    uppercased = map(str.upper, words)
     print(*uppercased)
 
 if __name__ == "__main__":
