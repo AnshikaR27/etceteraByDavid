b1dd8484c271d5df385595080c1b699949febc29
AnshikaR27 <94916495+AnshikaR27@users.noreply.github.com>
2025-02-19 15:45:07 +0530

    Learning about enumerate func

--- enumerators.py
+++ enumerators.py
@@ -1,4 +1,5 @@
 students = ["Hermoine", "Harry", "Ron"]
 
-for i in range(len(students)):
-    print(i + 1, students[i])
+for index, student in enumerate(students, start=1):
+    print(index, student)
+
